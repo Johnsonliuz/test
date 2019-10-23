77b6c34c2a18a4a2cd64dab6c670a65280098abf
Johnson <qaz54077@gmail.com>
2019-10-23 15:07:58 +0800

    two commit

--- project/test01.go
+++ project/test01.go
@@ -3,3 +3,18 @@
 //如果大於18歲則輸出"你的年齡大於18 要對自己的行為負責"
 //不到18歲則輸出"多練幾年再來"
 
+package main
+
+import "fmt"
+
+func main() {
+	var age int
+
+	fmt.Println("請輸入年齡:")
+	fmt.Scanln(&age)
+	if age > 18 {
+		fmt.Println("你的年齡大於18 要對自己的行為負責")
+	} else {
+		fmt.Println("多練幾年再來")
+	}
+}
