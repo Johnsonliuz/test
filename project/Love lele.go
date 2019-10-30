0402e2b2215a8e65b9d7343e5e3edf0b13a699ca
Johnson <qaz54077@gmail.com>
2019-10-30 23:20:39 +0800

    new code

---
+++ project/Love lele.go
@@ -0,0 +1,17 @@
+//請輸入在一起的日期
+//顯現出我愛樂樂
+
+package main
+
+import "fmt"
+
+func main() {
+    var date int
+    
+    fmt.Println("請輸入在一起的日期")
+    fmt.Scanf("%d", &date)
+    
+    if date == 20150913 {
+    fmt.Println("我愛樂樂")
+    }
+    }
