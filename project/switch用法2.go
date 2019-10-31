71144a9b0c77febfa6aded142cdc44b40b3e1ab1
Johnson <qaz54077@gmail.com>
2019-10-31 09:09:50 +0800

    switch 範例更新

---
+++ project/switch用法2.go
@@ -0,0 +1,20 @@
+//進階練習
+//學生成績大於等於60分輸出合格，低於60輸出不合格，成績不能高於100分
+
+package main
+
+import "fmt"
+
+func main() {
+	var score int
+	
+	fmt.Println("請輸入成績")
+	fmt.Scanf("%d", &score)
+	
+	switch int(score/60){
+		case 1 :
+			fmt.Println("合格")
+		case 0 :
+			fmt.Println("不合格")
+	}
+}
