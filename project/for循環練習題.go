9913ef865aa2c4a0d5d18e3339bd85ee6dd65ffb
Johnson <qaz54077@gmail.com>
2019-11-01 20:28:50 +0800

    for循環練習

---
+++ project/for循環練習題.go
@@ -0,0 +1,20 @@
+//練習
+//請打印出1~100所有9的倍數的整數各數及總和
+
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 1
+	var sum int
+	var count int
+	var max int
+	for ; i <= max; i++ {
+		if (i % 9) == 0 {
+		count++
+		sum += i
+		}
+		fmt.Printf("count=%v, sum=%v\n",count,sum)
+	}
+}/
