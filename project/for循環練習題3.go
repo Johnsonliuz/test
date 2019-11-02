58e4171cabbabafa63aa3b92a0cb5123957516fa
Johnson <qaz54077@gmail.com>
2019-11-02 16:50:25 +0800

    new

--- project/for循環練習題3.go
+++ project/for循環練習題3.go
@@ -1,6 +1,7 @@
 //練習
 //有三個班，每班有五個學生，求每個班的平均分數及總平均分數
 //學生成績用手動輸入
+//統計三個班及格人數
 
 package main
 
@@ -8,22 +9,28 @@ import "fmt"
 
 func main() {
 	
-	var class int
+	var classnum int = 3
+	var stunum int = 5
 	var score int
-	var sum int
-	var allsum int //三個班平均後班總和
+	var sum float64
+	var allsum float64 //三個班平均後班總和
+	var passcount int
 	
-	for class = 1; class <= 3; class++{
+	for j := 1; j <= classnum; j++{
 	sum = 0
-		for i := 1; i <= 5; i++{
-		fmt.Printf("請輸入第%d班第%d個學生的成績：",class,i)
+		for i:= 1; i <= stunum; i++{
+		fmt.Printf("請輸入第%d班第%d個學生的成績：",j,i)
 		fmt.Scanf("%d", &score)
-		sum += score
+		sum += float64(score)
+		if score >= 60{
+		passcount++
+		}
 	}
 	
-	fmt.Printf("第%v個班的平均分數=%v\n",class,sum/5)
-	allsum += sum/5
+	fmt.Printf("第%v個班的平均分數=%v\n",j,sum/5)
+	allsum += float64(sum/5)
 	}
 	
 	fmt.Printf("總平均=%v\n",allsum/3)
+	fmt.Printf("及格人數=%v\n",passcount)
 }
