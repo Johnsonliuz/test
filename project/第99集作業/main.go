2cdb343423654b0316291789ef34af1cf8d31410
Johnson <qaz54077@gmail.com>
2019-11-11 13:55:51 +0800

    new

--- project/第99集作業/main.go
+++ project/第99集作業/main.go
@@ -3,12 +3,58 @@ package main
 import "fmt"
 
 func main() {
+	var a int
+	var b int
+	var c int
+	var d int
 	
 	//實現判斷一個整數屬於哪個範圍 >0,<0,=0
 	
+	fmt.Println("請輸入一個整數：")
+	fmt.Scanf("%d",&a)
+	if a > 0 {
+		fmt.Println("正數")
+	} else if a < 0{
+		fmt.Println("負數")
+	} else if a == 0 {
+		fmt.Println("0")
+	}
+	
+	fmt.Println("------------------")
+	
+	
 	//判斷一個年份是否為閏年
 	
+	fmt.Println("請輸入一個年份：")
+	fmt.Scanf("%d",&a)
+	
+	if a % 4 == 0 {
+		fmt.Println("%d是閏年",a)
+	} else {
+		fmt.Println("%d不是閏年",a)
+	}
+	
+	fmt.Println("------------------")
+	
+	
 	//判斷一個整數是否為水仙花數
+	// 153 = 100*1+10*5+1*3
+	// 153 = 1*1*1+5*5*5+3*3*3
+	
+	fmt.Println("請輸入一個整數：")
+	fmt.Scanf("%d",&a)
+	b = a / 100
+	c = (a - 100*b) / 10
+	d = (a - 100*b - 10*c) / 1 
+	
+	if a = b*b*b+c*c*c+d*d*d && a = 100*b+10*c+1*d {
+		fmt.Println("是水仙花數")
+	} else {
+		fmt.Println("不是水仙花數")
+	}
+	
+	fmt.Println("------------------")
+	
 	
 	//寫出輸出結果
 	/* m := 0
