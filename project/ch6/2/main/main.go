9397e553bee56ed067cf6f881c52bba25cab9cd2
Johnson <qaz54077@gmail.com>
2019-11-16 15:07:17 +0800

    new

--- project/ch6/2/main/main.go
+++ project/ch6/2/main/main.go
@@ -1,3 +1,5 @@
+//請寫出一個可以自行輸入值並且輸出兩個數相加,相減的函數來調用
+
 package main
 
 import "fmt"
@@ -11,7 +13,13 @@ fnuc main() {
 	fmt.Println("請輸入第二個整數:")
 	fmt.Scanln(&b)
 	
-	res1,res2 := utils.Getsumandsub(1,2)
+	res1,res2 := utils.Getsumandsub(a,b)
 	fmt.Printf("res1=%v,res2=%v",res1,res2)
+	
+	
+	//如果只要調用第二個返回值，則可以運用底線_
+	
+	_,res3 := utils.Getsumandsub(a,b)
+	fmt.Printf("res3=%v",res3)
 	
 }
